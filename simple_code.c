6f4a8cb55d77a7fc3245dc0d4be809c04f904398
ahmadeltokhy600 <8ahmed8eltokhy8@gmail.com>
2023-12-13 08:40:04 -0800

    h

---
+++ simple_code.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ 
+*main - a simple code*/
+
+void main(void)
+{
+        int a = 10, b = 9;
+        int p,q;
+
+        p = &a;
+        q = &b;
+        printf("Value of a = %d\n", a);
+        printf("Value of a = %d\n", *p);
+}
